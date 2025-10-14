cd043bf400d736730d84d0e5f2a5c742bebe2bc1
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-14 01:59:23 +0100

    src/Statement.java

--- src/Statement.java
+++ src/Statement.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Statement {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Have a good day!");
+        System.out.println("Learning Java is fun!");
+    }
 }
