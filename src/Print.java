58812df69698256c742aad71134e1972b61c9791
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-15 02:12:55 +0100

    src/Comments.java

---
+++ src/Print.java
@@ -0,0 +1,9 @@
+// Print Text
+//You learned from the previous chapter that you can use the println() method to output values or print text in Java:
+
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
