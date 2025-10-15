90a00c859cd7f7d1942d077536dec3152160554e
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-15 02:14:18 +0100

    src/Statement.java

--- src/Statement.java
+++ src/Statement.java
@@ -5,3 +5,14 @@ public class Statement {
         System.out.println("Learning Java is fun!");
     }
 }
+
+// The statements are executed, one by one, in the same order as they are written:
+//
+// From the example above, we have three statements:
+//
+// System.out.println("Hello World!");
+// System.out.println("Have a good day!");
+// System.out.println("Learning Java is fun!");
+// The first statement is executed first (print "Hello World!" to the screen).
+// Then the second statement is executed (print "Have a good day!" to the screen).
+// And at last, the third statement is executed (print "Learning Java is fun!" to the screen)
