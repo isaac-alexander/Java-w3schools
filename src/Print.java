5188f6a284b6527a67229e31c1ddca59cc49a285
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-15 02:13:44 +0100

    src/Print.java

--- src/Print.java
+++ src/Print.java
@@ -1,9 +1,37 @@
 // Print Text
-//You learned from the previous chapter that you can use the println() method to output values or print text in Java:
+//You can use the println() method to output values or print text in Java:
+// You can add as many printIn() methods as you want. Note that it will add a new line for each method
+//
+//Double Quotes
+//Text must be wrapped inside double quotations marks "".
+//If you forget the double quotes, an error occurs:
 
 
-public class Output {
+//----------------------------------------------
+//The Print() Method
+//There is also a print() method, which is similar to println().
+//
+//The only difference is that it does not insert a new line at the end of the output
+// Note that we add an extra space (after "Hello World!" in the example below) for better readability.
+
+// ------------------------------------
+//Print Numbers
+//You can also use the println() method to print numbers.
+//
+//However, unlike text, we don't put numbers inside double quote
+// You can also perform mathematical calculations inside the println() method:
+
+public class Print {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
+
     }
 }
