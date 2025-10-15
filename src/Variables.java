6be43e20ae2019cc4f884a490dd7821a77040e30
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-15 02:14:40 +0100

    src/Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+// Java Variables
+//Variables are containers for storing data values.
+
+//In Java, there are different types of variables, for example:
+//String - stores text, such as "Hello". String values are surrounded by double quotes
+//int - stores integers (whole numbers), without decimals, such as 123 or -123
+//float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+//char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+//boolean - stores values with two states: true or false
+
+// Declaring (Creating) Variables
+
+//To create a variable in Java, you need to:
+//Choose a type (like int or String)
+//Give the variable a name (like x, age, or name)
+//Optionally assign it a value using =
+//Here's the basic syntax:
+
+// type variableName = value;
+// For example, if you want to store some text, you can use a String:
+//
+//Example
+//Create a variable called name of type String and assign it the value "John".
+//Then we use println() to print the name variable:
+
 
 public class Variables {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println(name);
+    }
 }
