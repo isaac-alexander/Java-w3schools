937dc798d1f32225ab938b5af6291347b6437dc6
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-19 02:20:59 +0100

    src/Characters.java

--- src/Characters.java
+++ src/Characters.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+// Characters
+
+// The char data type is used to store a single character. The character must be surrounded by single quotes, like 'A' or 'c':
+
+// Alternatively, if you are familiar with ASCII values, you can use those to display certain characters:
+// ASCII stands for the "American Standard Code for Information Interchange".
+
+// Strings
+// The String data type is used to store a sequence of characters (text). String values must be surrounded by double quotes:
+
+// Examples
+
 
 public class Characters {
+    static void main(String[] args) {
+        char myGrade = 'B';
+        System.out.println(myGrade);
+
+        // ASCII values
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+
+        // Strings
+        String greeting = "Hello World!";
+        System.out.println(greeting);
+    }
 }
