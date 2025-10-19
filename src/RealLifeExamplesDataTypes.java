ecc33890b05e1c1f9eeb4bf79e0954e26dc5e173
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-19 02:20:08 +0100

    src/RealLifeExamplesDataTypes.java

--- src/RealLifeExamplesDataTypes.java
+++ src/RealLifeExamplesDataTypes.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+// Here's a real-life example of using different data types, to calculate and output the total cost of a number of items:
+
 
 public class RealLifeExamplesDataTypes {
+    public static void main(String[] args) {
+        // Create variables of different data types
+        int items = 50;
+        float costPerItem = 9.99f;
+        float totalCost = items * costPerItem;
+        char currency = '$';
+
+        // Print variables
+        System.out.println("Number of items: " + items);
+        System.out.println("Cost per item: " + costPerItem + currency);
+        System.out.println("Total cost = " + totalCost + currency);
+    }
 }
