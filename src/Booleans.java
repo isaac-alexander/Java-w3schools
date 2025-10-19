d059c16d0992dcb05cdcb0526ad5311f3a25af7c
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-19 02:19:43 +0100

    src/Booleans.java

---
+++ src/Booleans.java
@@ -0,0 +1,17 @@
+// Boolean Types
+// Very often in programming, you will need a data type that can only have one of two values, like:
+
+// YES / NO
+// ON / OFF
+// TRUE / FALSE
+//For this, Java has a boolean data type, which can only take the values true or false:
+
+
+public class Booleans {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+    }
+}
