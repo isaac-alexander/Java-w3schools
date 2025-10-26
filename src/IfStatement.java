25b7823325738725c00b5f9eb265d254dfa82707
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-26 02:06:41 +0100

    src/IfStatement.java

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -30,7 +30,16 @@
 //        // block of code to be executed if the condition is true
 //        }
 
-//EXAMPLES //  Writing if (isLightOn) is the same as writing if (isLightOn == true), but shorter and easier to read.
+// If Without Braces
+//If an if statement has only one line of code, you can write it without curly braces { }:
+
+// Potential Problem
+// Without braces, only the first line after the if belongs to it. Any other lines will run no matter what, which can lead to unexpected results:
+
+// The Safe Way
+//To avoid mistakes, always use curly braces { }. This makes it clear which lines belong to the if statement:
+
+//EXAMPLES
 
 public class IfStatement {
     static void main(String[] args) {
@@ -64,5 +73,40 @@ public class IfStatement {
             System.out.println("The light is on.");
         }
 
+        boolean isLightOnNow = false;
+
+        if (isLightOnNow) {
+            System.out.println("The light is on now.");  // This will not be printed
+        }
+
+        System.out.println("This line always runs.");
+
+        // If Without Braces
+        //If an if statement has only one line of code, you can write it without curly braces { }:
+        if (20 > 18)
+            System.out.println("20 is greater than 18");
+
+        // Potential Problem (without curles braces{})
+        int x3 = 20;
+        int y3 = 18;
+
+        if (x3 > y3)
+            System.out.println("x3 is greater than y3");
+        System.out.println("This line runs no matter what (not part of the if statement)");
+
+        // The Safe Way. To avoid mistakes, always use curly braces { }
+        int x4 = 20;
+        int y4 = 18;
+
+        if (x4 > y4) {
+            System.out.println("x4 is greater than y4");
+            System.out.println("Both lines are part of the if");
+        }
+
+        System.out.println("I am outside if, not part of if!");
+
     }
 }
+// Note: Writing if (isLightOn) is the same as writing if (isLightOn == true), but shorter and easier to read.
+
+//Here is the same example with the value false to see that the program continues even when the code block does not run:
