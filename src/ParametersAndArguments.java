85f9da44799b64ac4666ab02cd237148bb00270f
Isaac Alexander <Isaacalexander81@gmail.com>
2025-10-29 02:01:54 +0100

    src/ParametersAndArguments.java

--- src/ParametersAndArguments.java
+++ src/ParametersAndArguments.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+// Parameters and Arguments
+// Information can be passed to methods as a parameter. Parameters act as variables inside the method.
+
+// Parameters are specified after the method name, inside the parentheses. You can add as many parameters as you want, just separate them with a comma.
+
+// The following example has a method that takes a String called fname as parameter. When the method is called, we pass along a first name, which is used inside the method to print the full name:
+
+// When a parameter is passed to the method, it is called an argument. So, from the example above: fname is a parameter, while Liam, Jenny and Anja are arguments.
+
 
 public class ParametersAndArguments {
+    static void myMethod(String fname) {
+        System.out.println(fname + " Refsnes");
+    }
+
+    public static void main(String[] args) {
+        myMethod("Liam");
+        myMethod("Jenny");
+        myMethod("Anja");
+    }
+
 }
