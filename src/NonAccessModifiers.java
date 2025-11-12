ebcd55972dad9f8ab80517183a7e520550b901e4
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-12 02:02:18 +0100

    src/NonAccessModifiers.java

--- src/NonAccessModifiers.java
+++ src/NonAccessModifiers.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+// Non-Access Modifiers: Non-access modifiers do not control visibility (like public or private), but instead add other features to classes, methods, and attributes.
+// The most commonly used non-access modifiers are final, static, and abstract.
+// Final: If you don't want the ability to override existing attribute values, declare attributes as final:
+
+
+
 
 public class NonAccessModifiers {
+    // final
+        final int x = 10;
+        final double PI = 3.14;
+
+        public static void main(String[] args) {
+            Main myObj = new Main();
+            myObj.x = 50; // will generate an error
+            myObj.PI = 25; // will generate an error
+            System.out.println(myObj.x);
+        }
 }
