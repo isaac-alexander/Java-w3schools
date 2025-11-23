ffe05c4bded85fad43c58e3e8f7efe46063dfb7b
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-23 01:52:26 +0100

    src/Enums.java

--- src/Enums.java
+++ src/Enums.java
@@ -1,4 +1,71 @@
-package PACKAGE_NAME;
+// An enum is a special "class" that represents a group of constants (unchangeable variables, like final variables).
+//
+//To create an enum, use the enum keyword (instead of class or interface), and separate the constants with a comma. Note that they should be in uppercase letters:
 
-public class Enum {
+// Enum is short for "enumerations", which means "specifically listed".
+
+
+//Example
+
+public class Enums {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    // Enum in a Switch Statement
+    //Enums are often used in switch statements to check for corresponding values:
+    enum Levels {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    public class Main {
+        public static void main(String[] args) {
+            Level myVar = Level.MEDIUM;
+            System.out.println(myVar);
+
+            Levels myVar2 = Levels.MEDIUM;
+
+            switch(myVar2) {
+                case LOW:
+                    System.out.println("Low level");
+                    break;
+                case MEDIUM:
+                    System.out.println("Medium level");
+                    break;
+                case HIGH:
+                    System.out.println("High level");
+                    break;
+            }
+        }
+    }
 }
+
+// Loop Through an Enum
+//The enum type has a values() method, which returns an array of all enum constants. This method is useful when you want to loop through the constants of an enum:
+//
+//Example
+// enum Level {
+//  LOW,
+//  MEDIUM,
+//  HIGH
+//}
+//
+//public class Main {
+//  public static void main(String[] args) {
+//    for (Level myVar : Level.values()) {
+//      System.out.println(myVar);
+//    }
+//  }
+//}
+
+
+// Difference between Enums and Classes
+//An enum can, just like a class, have attributes and methods. The only difference is that enum constants are public, static and final (unchangeable - cannot be overridden).
+//
+//An enum cannot be used to create objects, and it cannot extend other classes (but it can implement interfaces).
+//
+//Why And When To Use Enums?
+//Use enums when you have values that you know aren't going to change, like month days, days, colors, deck of cards, etc.
