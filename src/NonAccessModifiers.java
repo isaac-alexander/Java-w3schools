719d8dd2367b277fd72b01b1423af7383413397b
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-24 02:02:09 +0100

    src/NonAccessModifiers.java

--- src/NonAccessModifiers.java
+++ src/NonAccessModifiers.java
@@ -6,14 +6,13 @@
 
 
 public class NonAccessModifiers {
-    // final
         final int x = 10;
         final double PI = 3.14;
 
         public static void main(String[] args) {
-            Main myObj = new Main();
-            myObj.x = 50; // will generate an error
-            myObj.PI = 25; // will generate an error
-            System.out.println(myObj.x);
+            NonAccessModifiers myObj = new NonAccessModifiers();
+//            myObj.x = 50; // will generate an error
+//            myObj.PI = 25; // will generate an error
+//            System.out.println(myObj.x);
         }
 }
