de90fe63d9e822cb74594fc65f92b7e8bb43451e
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-29 02:01:57 +0100

    src/CreateFile.java

---
+++ src/CreateFile.java
@@ -0,0 +1,53 @@
+// Create a File
+//In Java, you can create a new file with the createNewFile() method from the File class.
+//
+//This method returns:
+//
+//true - if the file was created successfully
+//false - if the file already exists
+//Note that the method is enclosed in a try...catch block. This is necessary because it throws an IOException if an error occurs (if the file cannot be created for some reason):
+//
+
+//Example
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateAFile {
+        public static void main(String[] args) {
+            try {
+                File myObj = new File("filename.txt");
+                if (myObj.createNewFile()) {
+                    System.out.println("File created: " + myObj.getName());
+                } else {
+                    System.out.println("File already exists.");
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        } // OUTPUT: File created: filename.txt
+
+
+}
+
+// Explanation: The program tries to create a file called filename.txt. If the file does not exist, it will be created and a success message is printed. If the file already exists, you will see the message "File already exists." instead.
+//
+//Note: The createNewFile() method only creates an empty file. It does not add any content inside
+
+
+// Create a File in a Specific Folder
+//To create a file in a specific directory (requires permission), specify the path of the file and use double backslashes to escape the "\" character (for Windows). On Mac and Linux you can just write the path, like: /Users/name/filename.txt
+
+// try {
+//      File myObj = new File("C:\\Users\\MyName\\filename.txt");
+//      if (myObj.createNewFile()) {
+//        System.out.println("File created: " + myObj.getName());
+//        System.out.println("Absolute path: " + myObj.getAbsolutePath());
+//      } else {
+//        System.out.println("File already exists.");
+//      }
+//    } catch (IOException e) {
+//      System.out.println("An error occurred.");
+//      e.printStackTrace();
+//    }
