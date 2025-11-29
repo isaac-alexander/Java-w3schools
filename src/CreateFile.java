90cfd1075c464cbfadc7a1169f3b9fc5382c585c
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-29 02:02:56 +0100

    src/CreateFile.java

--- src/CreateFile.java
+++ src/CreateFile.java
@@ -13,7 +13,7 @@
 import java.io.File;
 import java.io.IOException;
 
-public class CreateAFile {
+public class CreateFile {
         public static void main(String[] args) {
             try {
                 File myObj = new File("filename.txt");
