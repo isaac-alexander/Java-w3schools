cad073dbb9eab7252911fb0251227b6b78e35014
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-10 02:12:26 +0100

    src/ListSortingTut.java

--- src/ListSortingTut.java
+++ src/ListSortingTut.java
@@ -1,4 +1,62 @@
-package PACKAGE_NAME;
+// Java Sort a List
+//In the previous chapters, you learned how to use two popular lists in Java: ArrayList and LinkedList, which are found in the java.util package.
+//
+//Another useful class in the java.util package is the Collections class, which include the sort() method for sorting lists alphabetically or numerically.
+//
+//Sort an ArrayList
+//Sort an ArrayList of Strings alphabetically in ascending order
+
+// Reverse the Order
+//You can also sort a list in reverse order, by using the reverseOrder() method.
+//
+//In the following example, we sort an ArrayList of Strings alphabetically in reverse/descending order:
+
+
+
+import java.util.ArrayList;
+import java.util.Collections;  // Import the Collections class
 
 public class ListSortingTut {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        Collections.sort(cars);
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+        // Reverse the Order
+        Collections.sort(cars, Collections.reverseOrder()); // Sort myNumbers
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+        // Sort an ArrayList of Integers numerically in ascending order:
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+        myNumbers.add(33);
+        myNumbers.add(15);
+        myNumbers.add(20);
+        myNumbers.add(34);
+        myNumbers.add(8);
+        myNumbers.add(12);
+
+        Collections.sort(myNumbers);
+
+        for (int i : myNumbers) {
+            System.out.println(i);
+        }
+
+        // Sort an ArrayList of Integers numerically in reverse/descending order:
+        Collections.sort(myNumbers, Collections.reverseOrder());
+
+        for (int i : myNumbers) {
+            System.out.println(i);
+        }
+    }
 }
