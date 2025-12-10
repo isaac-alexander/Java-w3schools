cd3a7e31e76bf68a953cf9962026ea382484ffd5
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-10 02:11:53 +0100

    src/ArrayListTut.java

--- src/ArrayListTut.java
+++ src/ArrayListTut.java
@@ -28,9 +28,25 @@
 // Change an Element
 //To modify an element, use the set() method and refer to the index number:
 
+// Remove an Element
+//To remove an element, use the remove() method and refer to the index number:
+
+// ArrayList Size
+//To find out how many elements an ArrayList have, use the size method:
+
+// Loop Through an ArrayList
+//Loop through the elements of an ArrayList with a for loop, and use the size() method to specify how many times the loop should run:
+
+
+// Sort an ArrayList
+//Another useful class in the java.util package is the Collections class, which include the sort() method for sorting lists alphabetically or numerically:
+
+
 // EXAMPLES
 
 import java.util.ArrayList; // Import the ArrayList class
+import java.util.Collections;  // Import the Collections class to sort arraylist
+
 public class ArrayListTut {
     public static void main(String[] args) {
         ArrayList<String> cars = new ArrayList<String>();
@@ -49,5 +65,72 @@ public class ArrayListTut {
         System.out.println(cars);
 
         System.out.println(cars.get(0)); // Get the first element
+
+        cars.remove(0); // Remove an element
+        System.out.println(cars);
+
+        // To remove all the elements in the ArrayList, use the clear() method:
+        // cars.clear();
+
+        // ArrayList Size
+        //To find out how many elements an ArrayList have, use the size method:
+        System.out.println(cars.size());
+
+        // Loop Through an ArrayList
+        // Loop through the elements of an ArrayList with a for loop, and use the size() method to specify how many times the loop should run:
+
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.println(cars.get(i) + " for loop with size");
+        }
+
+        // You can also loop through an ArrayList with the for-each loop:
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+        Collections.sort(cars);
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+        // Create an ArrayList to store numbers (add elements of type Integer):
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+        myNumbers.add(10);
+        myNumbers.add(15);
+        myNumbers.add(20);
+        myNumbers.add(25);
+        for (int i : myNumbers) {
+            System.out.println(i);
+        }
+        Collections.sort(myNumbers);
+
+        for (int i : myNumbers) {
+            System.out.println(i);
+        }
     }
 }
+//
+//Other Types
+//Elements in an ArrayList are actually objects. In the examples above, we created elements (objects) of type "String". Remember that a String in Java is an object (not a primitive type). To use other types, such as int, you must specify an equivalent wrapper class: Integer. For other primitive types, use: Boolean for boolean, Character for char, Double for double, etc:
+
+// The var Keyword
+//From Java 10, you can use the var keyword to declare an ArrayList variable without writing the type twice. The compiler figures out the type from the value you assign.
+//
+//This makes code shorter, but many developers still use the full type for clarity. Since var is valid Java, you may see it in other code, so it's good to know that it exists:
+
+// import java.util.ArrayList;
+//
+//public class Main {
+//  public static void main(String[] args) {
+//    // Using var instead of
+//    // ArrayList<String> cars = new ArrayList<String>();
+//    var cars = new ArrayList<String>(); // using var
+//    cars.add("Volvo");
+//    cars.add("BMW");
+//    cars.add("Ford");
+//    cars.add("Mazda");
+//    System.out.println(cars);
+//  }
+//}
+
