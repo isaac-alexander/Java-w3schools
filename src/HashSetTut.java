fd6254c56909f12f234e5678e5e6118f6f8733f8
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-25 02:10:07 +0100

    src/Set.java

---
+++ src/HashSetTut.java
@@ -0,0 +1,22 @@
+// Java HashSet
+// A HashSet is a collection of elements where every element is unique.
+
+// It is part of the java.util package and implements the Set interface.
+
+// Create a HashSet ....Example
+//Create a HashSet object called cars that will store strings:
+
+// import java.util.HashSet; // Import the HashSet class
+
+// HashSet<String> cars = new HashSet<String>();
+
+// Now you can use methods like add(), contains(), and remove() to manage your collection of unique elements.
+
+// Add Elements
+//To add elements to a HashSet, use the add() method:
+//
+//Example
+
+
+public class HashSet {
+}
