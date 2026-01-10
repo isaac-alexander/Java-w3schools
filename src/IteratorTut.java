a73ea6c3332c2d41d63f787f2edec0841e7a9da0
Isaac Alexander <Isaacalexander81@gmail.com>
2026-01-10 01:59:12 +0100

    src/IteratorTut.java

--- src/IteratorTut.java
+++ src/IteratorTut.java
@@ -1,4 +1,77 @@
-package PACKAGE_NAME;
+// Java Iterator
+// An Iterator is an object that can be used to loop through collections, like ArrayList and HashSet.
+
+// It is called "iterator" because "iterating" is the technical term for looping.
+// To use an iterator,  you must import it from the java.util package.
+
+// Getting an Iterator
+//The iterator() method can be used to get an Iterator for any collection:
+
+
+// The var Keyword with Iterators
+//You can also use the var keyword with iterators. This avoids repeating the long type name Iterator<String>, since the compiler already knows the type from the collection.
+
+
+
+//Example
+
+// Import the ArrayList class and the Iterator class
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class IteratorTut {
+    public static void main(String[] args) {
+
+        // Make a collection
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        // Get the iterator
+        Iterator<String> it = cars.iterator();
+
+        // Print the first item
+        System.out.println(it.next());
+
+        // Looping Through a Collection
+        //To loop through a collection, use the hasNext() and next() methods of the Iterator:
+        // Loop through a collection
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        // Removing Items from a Collection
+        //Iterators are designed to easily change the collections that they loop through. The remove() method can remove items from a collection while looping.
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(12);
+        numbers.add(8);
+        numbers.add(2);
+        numbers.add(23);
+        Iterator<Integer> its = numbers.iterator();
+        while(its.hasNext()) {
+            Integer i = its.next();
+            if(i < 10) {
+                its.remove();
+            }
+        }
+        System.out.println(numbers);
+        // Note: Trying to remove items using a for loop or a for-each loop would not work correctly because the collection is changing size at the same time that the code is trying to loop.
+
+        // The var Keyword with Iterators
+        // Make a collection with var
+        var cars2 = new ArrayList<String>();
+        cars2.add("Volvo");
+        cars2.add("BMW");
+        cars2.add("Ford");
+        cars2.add("Mazda");
+
+        // Get the iterator using var
+        var it2 = cars.iterator();
+
+        // Print the first item
+        System.out.println(it2.next());
+        // Here, var makes the iterator declaration shorter, but the actual type is still Iterator<String>.
+    }
 }
